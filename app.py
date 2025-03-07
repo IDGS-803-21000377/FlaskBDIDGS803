7653476d0264aad71d03ee2e03fe889f673f272e
LuisGerardoLopez <21000377@alumnos.utleon.edu.mx>
2025-03-06 22:26:02 -0600

    agregar

--- app.py
+++ app.py
@@ -15,8 +15,34 @@ csrf = CSRFProtect()
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	create_from = forms.UserForm2(request.form)
+	alumno = Alumnos.query.all()
 
+	return render_template("index.html",form= create_from,alumnos=alumno)
+
+@app.route("/detalles", methods=["GET","POST"])
+def detalles():
+	create_form=forms.UserForm2(request.form)
+	if request.method=='GET':
+		id=request.args.get('id')
+		alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		id = request.args.get('id')
+		nombre = alum1.nombre
+		apaterno = alum1.apaterno
+		email = alum1.email
+		return render_template("detalles.html", id=id, nombre=nombre, apaterno=apaterno, email=email)
+  
+@app.route("/Alumnos1",methods=['GET','POST'])
+def Alumnos():
+  create_from = forms.UserForm2(request.form)
+  if request.method == 'POST':
+   alum = Alumnos(nombre= create_from.nombre.data,
+                  apaterno=create_from.apaterno.data,
+                  email=create_from.email.data)
+   db.session.add(alum)
+   db.session.commit()
+   return redirect(url_for('index'))
+  return render_template('Alumnos.html',form=create_from)
 if __name__ == '__main__':
 	csrf.init_app(app)
 
