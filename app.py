f01ec0c4e129756340f523432c6829a7bb8556f1
LuisGerardoLopez <21000377@alumnos.utleon.edu.mx>
2025-03-07 09:39:03 -0600

    agregar

--- app.py
+++ app.py
@@ -1,53 +1,53 @@
-from flask import Flask, render_template,request,redirect,url_for
-from flask import Flask
-from  flask_wtf.csrf import CSRFProtect
-from flask import g
-import forms
-from models import db
-from models import Alumnos
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf.csrf import CSRFProtect
+from models import db, Alumnos
 from config import DevelomentConfig
+import forms
 
 app = Flask(__name__)
 app.config.from_object(DevelomentConfig)
-csrf = CSRFProtect()
+csrf = CSRFProtect(app)  
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-	create_from = forms.UserForm2(request.form)
-	alumno = Alumnos.query.all()
+    create_form = forms.UserForm2(request.form)
+    alumnos = Alumnos.query.all()
+    return render_template("index.html", form=create_form, alumnos=alumnos)
 
-	return render_template("index.html",form= create_from,alumnos=alumno)
-
-@app.route("/detalles", methods=["GET","POST"])
+@app.route("/detalles", methods=["GET", "POST"])
 def detalles():
-	create_form=forms.UserForm2(request.form)
-	if request.method=='GET':
-		id=request.args.get('id')
-		alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
-		id = request.args.get('id')
-		nombre = alum1.nombre
-		apaterno = alum1.apaterno
-		email = alum1.email
-		return render_template("detalles.html", id=id, nombre=nombre, apaterno=apaterno, email=email)
-  
-@app.route("/Alumnos1",methods=['GET','POST'])
-def Alumnos():
-  create_from = forms.UserForm2(request.form)
-  if request.method == 'POST':
-   alum = Alumnos(nombre= create_from.nombre.data,
-                  apaterno=create_from.apaterno.data,
-                  email=create_from.email.data)
-   db.session.add(alum)
-   db.session.commit()
-   return redirect(url_for('index'))
-  return render_template('Alumnos.html',form=create_from)
+    create_form = forms.UserForm2(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        if id:
+            try:
+                id = int(id)  
+                alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+                if alum1:
+                    return render_template("detalles.html", id=id, nombre=alum1.nombre, apaterno=alum1.apaterno, email=alum1.email)
+            except ValueError:
+                pass  
+    return redirect(url_for('index'))  
+
+
+@app.route("/Alumnos1", methods=['GET', 'POST'])
+def registrar_alumno():
+    create_form = forms.UserForm2(request.form)
+    if request.method == 'POST' and create_form.validate():
+        nuevo_alumno = Alumnos(
+            nombre=create_form.nombre.data,
+            apaterno=create_form.apaterno.data,
+            email=create_form.email.data
+        )
+        db.session.add(nuevo_alumno)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('Alumnos.html', form=create_form)
+
 if __name__ == '__main__':
-	csrf.init_app(app)
-
-	db.init_app(app)
-	with app.app_context():
-		db.create_all()
-
-	app.run()
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()  
+    app.run(debug=True)  
